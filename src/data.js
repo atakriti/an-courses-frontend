7d83081a9e04efa8b8956b15a07476b523e0c3fc
atakriti <anwart256@gmail.com>
2023-01-06 15:34:09 +0100

    finished 40 obejcts

---
+++ src/data.js
@@ -0,0 +1,481 @@
+let data = [
+    {
+        question: "__ heiße Maria",
+        translate: "اسمي ماريا",
+        type: "grammer",
+        level:"a1",
+        lan: "de",
+        options: [
+            {answer:"Ich",isCorrect:true},
+            {answer:"du",isCorrect:false},
+            {answer:"sie",isCorrect:false},
+            {answer:"ihr",isCorrect:false}
+        ]
+    },
+    {
+        question: "__ geht es dir?",
+        translate: "كيفك؟",
+        type: "grammer",
+        level:"a1",
+        lan: "de",
+        options: [
+            {answer:"was",isCorrect:false},
+            {answer:"wo",isCorrect:false},
+            {answer:"wie",isCorrect:true},
+            {answer:"wann",isCorrect:false}
+        ]
+    },
+    {
+        question: "Du __ in Berlin.",
+        translate: "انت ساكن في برلين",
+        type: "grammer",
+        level:"a1",
+        lan: "de",
+        options: [
+            {answer:"hast",isCorrect:false},
+            {answer:"wohnst",isCorrect:true},
+            {answer:"was",isCorrect:false},
+            {answer:"wann",isCorrect:false}
+        ]
+    },
+    {
+        question: "Ich spreche __ Deutsch",
+        translate: "انا ما بحكي الماني",
+        type: "grammer",
+        level:"a1",
+        lan: "de",
+        options: [
+            {answer:"nein",isCorrect:false},
+            {answer:"nichts",isCorrect:false},
+            {answer:"nicht",isCorrect:false},
+            {answer:"kein",isCorrect:true}
+        ]
+    },
+    {
+        question: "Ich __ Durst",
+        translate: "انا عطشان",
+        type: "grammer",
+        level:"a1",
+        lan: "de",
+        options: [
+            {answer:"habe",isCorrect:true},
+            {answer:"bin",isCorrect:false},
+            {answer:"kann",isCorrect:false},
+            {answer:"haben",isCorrect:false}
+        ]
+    },
+    {
+        question: "Ich __ Hunger.",
+        translate: "انا جوعان",
+        type: "grammer",
+        level:"a1",
+        lan: "de",
+        options: [
+            {answer:"habe",isCorrect:true},
+            {answer:"bin",isCorrect:false},
+            {answer:"kann",isCorrect:false},
+            {answer:"haben",isCorrect:false}
+        ]
+    },
+    {
+        question: "Ich __ müde.",
+        translate: "انا تعبان",
+        type: "grammer",
+        level:"a1",
+        lan: "de",
+        options: [
+            {answer:"habe",isCorrect:false},
+            {answer:"haben",isCorrect:false},
+            {answer:"bin",isCorrect:true},
+            {answer:"sein",isCorrect:false}
+        ]
+    },
+    {
+        question: "Du __ nett.",
+        translate: "انت ظريف",
+        type: "grammer",
+        level:"a1",
+        lan: "de",
+        options: [
+            {answer:"bist",isCorrect:true},
+            {answer:"hast",isCorrect:false},
+            {answer:"haben",isCorrect:false},
+            {answer:"sein",isCorrect:false}
+        ]
+    },
+    {
+        question: "Ihr __ zu spät.",
+        translate: "انتو متاخرين",
+        type: "grammer",
+        level:"a1",
+        lan: "de",
+        options: [
+            {answer:"habt",isCorrect:false},
+            {answer:"haben",isCorrect:false},
+            {answer:"sind",isCorrect:false},
+            {answer:"seid",isCorrect:true}
+        ]
+    },
+    {
+        question: "Er __ krank.",
+        translate: "هو مريض",
+        type: "grammer",
+        level:"a1",
+        lan: "de",
+        options: [
+            {answer:"habe",isCorrect:false},
+            {answer:"ist",isCorrect:true},
+            {answer:"sind",isCorrect:false},
+            {answer:"hat",isCorrect:false}
+        ]
+    },
+    {
+        question: "__ ist kalt draußen.",
+        translate: "برا برد",
+        type: "grammer",
+        level:"a1",
+        lan: "de",
+        options: [
+            {answer:"Hat",isCorrect:false},
+            {answer:"Es",isCorrect:true},
+            {answer:"Er",isCorrect:false},
+            {answer:"Sind",isCorrect:false}
+        ]
+    },
+    {
+        question: "Wir __ hungrig.",
+        translate: "نحنا جوعانين",
+        type: "grammer",
+        level:"a1",
+        lan: "de",
+        options: [
+            {answer:"sehr",isCorrect:false},
+            {answer:"haben",isCorrect:false},
+            {answer:"sind",isCorrect:true},
+            {answer:"habe",isCorrect:false}
+        ]
+    },
+    {
+        question: "__ machst du heute Abend? ",
+        translate: "شو عندك اليوم المسا؟",
+        type: "grammer",
+        level:"a1",
+        lan: "de",
+        options: [
+            {answer:"Was",isCorrect:true},
+            {answer:"Wo",isCorrect:false},
+            {answer:"Wann",isCorrect:false},
+            {answer:"Wie",isCorrect:false}
+        ]
+    },
+    {
+        question: "__ wohnst du? ",
+        translate: "وين ساكن انت؟",
+        type: "grammer",
+        level:"a1",
+        lan: "de",
+        options: [
+            {answer:"Was",isCorrect:false},
+            {answer:"Wo",isCorrect:true},
+            {answer:"Wann",isCorrect:false},
+            {answer:"Wie",isCorrect:false}
+        ]
+    },
+    {
+        question: "__ fängt der Film an?",
+        translate: "امتى لح يبلش الفلم؟",
+        type: "grammer",
+        level:"a1",
+        lan: "de",
+        options: [
+            {answer:"Was",isCorrect:false},
+            {answer:"Wo",isCorrect:false},
+            {answer:"Wann",isCorrect:true},
+            {answer:"Wie",isCorrect:false}
+        ]
+    },
+    {
+        question: "__ bist du traurig?",
+        translate: "ليش زعلان؟",
+        type: "grammer",
+        level:"a1",
+        lan: "de",
+        options: [
+            {answer:"Was",isCorrect:false},
+            {answer:"Wo",isCorrect:false},
+            {answer:"Wann",isCorrect:false},
+            {answer:"Warum",isCorrect:true}
+        ]
+    },
+    {
+        question: "__ kommst du zu spät ?",
+        translate: "ليش متاخر؟",
+        type: "grammer",
+        level:"a1",
+        lan: "de",
+        options: [
+            {answer:"Was",isCorrect:false},
+            {answer:"Wo",isCorrect:false},
+            {answer:"Wann",isCorrect:false},
+            {answer:"Wieso",isCorrect:true}
+        ]
+    },
+    // ============================================================= A1 Vocabs ===========
+    {
+        question: "Ich __ aus Syrien",
+        translate: "انا من سوريا",
+        type: "vocabs",
+        level:"a1",
+        lan: "de",
+        options: [
+            {answer:"kann",isCorrect:false},
+            {answer:"kome",isCorrect:false},
+            {answer:"liebe",isCorrect:false},
+            {answer:"komme",isCorrect:true}
+        ]
+    },
+    {
+        question: "Ich __ ein __ Deutsch",
+        translate: "انا بحكي شوية الماني",
+        type: "vocabs",
+        level:"a1",
+        lan: "de",
+        options: [
+            {answer:"sprechen,bischen",isCorrect:false},
+            {answer:"spreche,bisschen",isCorrect:true},
+            {answer:"sprachen,bischen",isCorrect:false},
+            {answer:"spreache,bisschen",isCorrect:false}
+        ]
+    },
+    {
+        question: "Ich __ gerne Deutsch",
+        translate: "انا عم اتعلم الماني",
+        type: "vocabs",
+        level:"a1",
+        lan: "de",
+        options: [
+            {answer:"lernen",isCorrect:false},
+            {answer:"Lerne",isCorrect:false},
+            {answer:"lerne",isCorrect:true},
+            {answer:"lernt",isCorrect:false}
+        ]
+    },
+    {
+        question: "Ich __ in einem Haus",
+        translate: "انا بعيش ببيت",
+        type: "vocabs",
+        level:"a1",
+        lan: "de",
+        options: [
+            {answer:"wohne",isCorrect:true},
+            {answer:"Wohne",isCorrect:false},
+            {answer:"wone",isCorrect:false},
+            {answer:"Wone",isCorrect:false}
+        ]
+    },
+    {
+        question: "Ich __ gerne Brot und Käse",
+        translate: "انا باكل جبنة و خبز",
+        type: "vocabs",
+        level:"a1",
+        lan: "de",
+        options: [
+            {answer:"Esse",isCorrect:false},
+            {answer:"ese",isCorrect:false},
+            {answer:"Ese",isCorrect:false},
+            {answer:"esse",isCorrect:true}
+        ]
+    },
+    {
+        question: "Ich __ gerne Wasser",
+        translate: "انا بشرب مي",
+        type: "vocabs",
+        level:"a1",
+        lan: "de",
+        options: [
+            {answer:"trinke",isCorrect:true},
+            {answer:"Trinke",isCorrect:false},
+            {answer:"Drink",isCorrect:false},
+            {answer:"drinke",isCorrect:false}
+        ]
+    },
+    {
+        question: "Ich möchte gerne ins __ gehen",
+        translate: "انا بدي روح على السينما",
+        type: "vocabs",
+        level:"a1",
+        lan: "de",
+        options: [
+            {answer:"kino",isCorrect:false},
+            {answer:"Kino",isCorrect:true},
+            {answer:"schule",isCorrect:false},
+            {answer:"fahrad",isCorrect:false}
+        ]
+    },
+    {
+        question: "Ich habe morgen __",
+        translate: "انا بكرا عندي مدرسة",
+        type: "vocabs",
+        level:"a1",
+        lan: "de",
+        options: [
+            {answer:"schule",isCorrect:false},
+            {answer:"auto",isCorrect:false},
+            {answer:"kino",isCorrect:false},
+            {answer:"Schule",isCorrect:true}
+        ]
+    },
+    {
+        question: "Ich bin __",
+        translate: "انا مبسوط",
+        type: "vocabs",
+        level:"a1",
+        lan: "de",
+        options: [
+            {answer:"fröhlich",isCorrect:true},
+            {answer:"mode",isCorrect:false},
+            {answer:"gluklich",isCorrect:false},
+            {answer:"frolich",isCorrect:false}
+        ]
+    },
+    {
+        question: "Ich bin __",
+        translate: "انا مشغول",
+        type: "vocabs",
+        level:"a1",
+        lan: "de",
+        options: [
+            {answer:"beschäftige",isCorrect:false},
+            {answer:"bescheftigt",isCorrect:false},
+            {answer:"beschäftigt",isCorrect:true},
+            {answer:"beshäftigt",isCorrect:false}
+        ]
+    },
+    {
+        question: "Ich bin im __",
+        translate: "انا بالمول",
+        type: "vocabs",
+        level:"a1",
+        lan: "de",
+        options: [
+            {answer:"Einkaufzentrum",isCorrect:false},
+            {answer:"Einkaufszentrum",isCorrect:true},
+            {answer:"einkaufszentrum",isCorrect:false},
+            {answer:"einkauf",isCorrect:false}
+        ]
+    },
+    {
+        question: "Ich bin im __",
+        translate: "انا بالمتحف",
+        type: "vocabs",
+        level:"a1",
+        lan: "de",
+        options: [
+            {answer:"musum",isCorrect:false},
+            {answer:"museum",isCorrect:false},
+            {answer:"Musum",isCorrect:false},
+            {answer:"Museum",isCorrect:true}
+        ]
+    },
+    // ==================================== Writting =========================
+    {
+        question: "heiße/Maria/Ich",
+        translate: "اسمي ماريا",
+        type: "writting",
+        level:"a1",
+        lan: "de",
+       answer:"Ich heiße Maria"
+    },
+    {
+        question: "kein/spreche/Deutsch/Ich",
+        translate: "انا ما بحكي الماني",
+        type: "writting",
+        level:"a1",
+        lan: "de",
+       answer:"Ich spreche kein Deutsch"
+    },
+    {
+        question: "ist/Es/draußen/kalt",
+        translate: "برا برد",
+        type: "writting",
+        level:"a1",
+        lan: "de",
+       answer:"Es ist kalt draußen"
+    },
+    {
+        question: "sind/hungrig/Wir",
+        translate: "نحنا جوعانين",
+        type: "writting",
+        level:"a1",
+        lan: "de",
+       answer:"Wir sind hungrig"
+    },
+    {
+        question: "heute/machst/Abend/Was/du/?",
+        translate: "شو عندك اليوم المسا؟",
+        type: "writting",
+        level:"a1",
+        lan: "de",
+       answer:"Was machst du heute Abend?"
+    },
+    {
+        question: "an/der/fängt/Film/Wann/?",
+        translate: "امتى لح يبلش الفلم؟",
+        type: "writting",
+        level:"a1",
+        lan: "de",
+       answer:"Wann fängt der Film an?"
+    },
+    {
+        question: "zu/du/spät/Wieso/kommst?",
+        translate: "ليش متاخر؟",
+        type: "writting",
+        level:"a1",
+        lan: "de",
+       answer:"Wieso kommst du zu spät?"
+    },
+    {
+        question: "Deutsch/spreche/ein/Ich/bisschen",
+        translate: "انا بحكي شوية الماني",
+        type: "writting",
+        level:"a1",
+        lan: "de",
+       answer:"Ich spreche ein bisschen Deutsch"
+    },
+    {
+        question: "Ich/Kino/gerne/möchte/gehe/ins",
+        translate: "انا بدي روح على السينما",
+        type: "writting",
+        level:"a1",
+        lan: "de",
+       answer:"Ich möchte gerne ins Kino gehe"
+    },
+    // =============================================== A2 ========================
+    {
+        question: "Ich sehe __ Katze __ Garten",
+        translate: "انا شايف القطة بالحديقة",
+        type: "grammer",
+        level:"a2",
+        lan: "de",
+        options: [
+            {answer:"die,im",isCorrect:true},
+            {answer:"das,in",isCorrect:false},
+            {answer:"der,in",isCorrect:false},
+            {answer:"den,im",isCorrect:false}
+        ]
+    },
+    {
+        question: "Ich habe __ Buch auf __ Tisch gelesen",
+        translate: "انا قرات الكتاب على الطاولة",
+        type: "grammer",
+        level:"a2",
+        lan: "de",
+        options: [
+            {answer:"die",isCorrect:false},
+            {answer:"das",isCorrect:false},
+            {answer:"das,dem",isCorrect:true},
+            {answer:"den",isCorrect:false}
+        ]
+    },
+]
+console.log(data.length);
