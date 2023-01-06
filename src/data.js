fffa1796f89e79116c64109f916a1f9094178fbc
atakriti <anwart256@gmail.com>
2023-01-06 19:14:23 +0100

    finished 87 objects

--- src/data.js
+++ src/data.js
@@ -471,11 +471,458 @@ let data = [
         level:"a2",
         lan: "de",
         options: [
-            {answer:"die",isCorrect:false},
-            {answer:"das",isCorrect:false},
+            {answer:"die,dem",isCorrect:false},
+            {answer:"das,den",isCorrect:false},
             {answer:"das,dem",isCorrect:true},
-            {answer:"den",isCorrect:false}
+            {answer:"den,der",isCorrect:false}
         ]
     },
+    {
+        question: "Wir hören __ Musik __ Auto",
+        translate: "عم نسمع الاغاني بالسيارة ",
+        type: "grammer",
+        level:"a2",
+        lan: "de",
+        options: [
+            {answer:"die,im",isCorrect:true},
+            {answer:"das,im",isCorrect:false},
+            {answer:"das,in",isCorrect:false},
+            {answer:"den,im",isCorrect:false}
+        ]
+    },
+    {
+        question: "Er trinkt __ Kaffee schnell",
+        translate: "عم يشرب القهوة بسرعة ",
+        type: "grammer",
+        level:"a2",
+        lan: "de",
+        options: [
+            {answer:"die",isCorrect:false},
+            {answer:"den",isCorrect:true},
+            {answer:"das",isCorrect:false},
+            {answer:"dem",isCorrect:false}
+        ]
+    },
+    {
+        question: "Ich habe __ Handy verloren",
+        translate: "ضيعت موبايلي",
+        type: "grammer",
+        level:"a2",
+        lan: "de",
+        options: [
+            {answer:"die",isCorrect:false},
+            {answer:"den",isCorrect:false},
+            {answer:"das",isCorrect:true},
+            {answer:"dem",isCorrect:false}
+        ]
+    },
+    {
+        question: "Sie gibt __ Baby die Flasche",
+        translate: "عم تعطي الطفل القنينة",
+        type: "grammer",
+        level:"a2",
+        lan: "de",
+        options: [
+            {answer:"die",isCorrect:false},
+            {answer:"den",isCorrect:false},
+            {answer:"das",isCorrect:false},
+            {answer:"dem",isCorrect:true}
+        ]
+    },
+    {
+        question: "Er schreibt die E-Mail auf __ Computer",
+        translate: "عم يكتب الايميل على الكمبيوتر",
+        type: "grammer",
+        level:"a2",
+        lan: "de",
+        options: [
+            {answer:"die",isCorrect:false},
+            {answer:"den",isCorrect:false},
+            {answer:"das",isCorrect:false},
+            {answer:"dem",isCorrect:true}
+        ]
+    },
+    {
+        question: "Ich nehme die Jacke __ Haken",
+        translate: "عم ياخود الجاكيت من العليقة",
+        type: "grammer",
+        level:"a2",
+        lan: "de",
+        options: [
+            {answer:"vom",isCorrect:true},
+            {answer:"von",isCorrect:false},
+            {answer:"auf",isCorrect:false},
+            {answer:"auf den",isCorrect:false}
+        ]
+    },
+    {
+        question: "Sie trägt die Tasche über __ Schulter",
+        translate: "هية حاملة الشنتاية على الكتف",
+        type: "grammer",
+        level:"a2",
+        lan: "de",
+        options: [
+            {answer:"den",isCorrect:false},
+            {answer:"die",isCorrect:false},
+            {answer:"der",isCorrect:true},
+            {answer:"das",isCorrect:false}
+        ]
+    },
+    {
+        question: "Die Blume gehört __ Mädchen",
+        translate: "الوردة للبنت",
+        type: "grammer",
+        level:"a2",
+        lan: "de",
+        options: [
+            {answer:"dem",isCorrect:true},
+            {answer:"die",isCorrect:false},
+            {answer:"der",isCorrect:false},
+            {answer:"das",isCorrect:false}
+        ]
+    },
+    {
+        question: "Der Ball ist unter __ Sofa",
+        translate: "الطابة تحت الصوفا",
+        type: "grammer",
+        level:"a2",
+        lan: "de",
+        options: [
+            {answer:"die",isCorrect:false},
+            {answer:"der",isCorrect:false},
+            {answer:"dem",isCorrect:true},
+            {answer:"das",isCorrect:false}
+        ]
+    },
+    {
+        question: "Der Lehrer gibt __ Schüler eine Aufgabe",
+        translate: "الاستاذ عم يعطي الطالب وظيفة",
+        type: "grammer",
+        level:"a2",
+        lan: "de",
+        options: [
+            {answer:"die",isCorrect:false},
+            {answer:"dem",isCorrect:true},
+            {answer:"der",isCorrect:false},
+            {answer:"das",isCorrect:false}
+        ]
+    },
+    {
+        question: "Der Apfel fällt __ Baum",
+        translate: "التفاحة تسقط من على الشجرة",
+        type: "grammer",
+        level:"a2",
+        lan: "de",
+        options: [
+            {answer:"über",isCorrect:false},
+            {answer:"auf",isCorrect:false},
+            {answer:"von",isCorrect:false},
+            {answer:"vom",isCorrect:true}
+        ]
+    },
+    {
+        question: "Ich gebe __ Katz__ Futter",
+        translate: "عم طعمي القطة",
+        type: "grammer",
+        level:"a2",
+        lan: "de",
+        options: [
+            {answer:"die,en",isCorrect:false},
+            {answer:"den,en",isCorrect:true},
+            {answer:"der,en",isCorrect:false},
+            {answer:"die,e",isCorrect:false},
+        ]
+    },
+    {
+        question: "Die Kinder bekommen Geschenke __ __ Eltern",
+        translate: "الاولاد عم يتلقون هدايا من الاهل",
+        type: "grammer",
+        level:"a2",
+        lan: "de",
+        options: [
+            {answer:"vom die",isCorrect:false},
+            {answer:"von den",isCorrect:true},
+            {answer:"von der",isCorrect:false},
+            {answer:"von die",isCorrect:false},
+        ]
+    },
+    {
+        question: "Wir danken __ Nachbar_ für ihre Hilfe",
+        translate: "نحنا منشكر الجيران لمساعدتهم",
+        type: "grammer",
+        level:"a2",
+        lan: "de",
+        options: [
+            {answer:"der,-",isCorrect:false},
+            {answer:"die,n",isCorrect:false},
+            {answer:"den,n",isCorrect:true},
+            {answer:"die,-",isCorrect:false},
+        ]
+    },
+    {
+        question: "Ich gebe __ Frau eine Blume",
+        translate: "عم اعطي المرأة وردة",
+        type: "grammer",
+        level:"a2",
+        lan: "de",
+        options: [
+            {answer:"der",isCorrect:true},
+            {answer:"die",isCorrect:false},
+            {answer:"den",isCorrect:false},
+            {answer:"dem",isCorrect:false},
+        ]
+    },
+    {
+        question: "Die Schwester schenkt __ Mutter eine Karte",
+        translate: "الاخت تهدي الام بطاقة",
+        type: "grammer",
+        level:"a2",
+        lan: "de",
+        options: [
+            {answer:"die",isCorrect:false},
+            {answer:"der",isCorrect:true},
+            {answer:"den",isCorrect:false},
+            {answer:"dem",isCorrect:false},
+        ]
+    },
+    // ======================================= A2 Vocabs ======================
+    {
+        question: "Wir fahren in den __ nach Italien.",
+        translate: "نحنا مسافرين بالعطلة على ايطاليا",
+        type: "vocabs",
+        level:"a2",
+        lan: "de",
+        options: [
+            {answer:"hause",isCorrect:false},
+            {answer:"schule",isCorrect:false},
+            {answer:"ferien",isCorrect:false},
+            {answer:"Ferien",isCorrect:true}
+        ]
+    },
+    {
+        question: "Der Hund meiner Nachbarn __ immer sehr laut",
+        translate: "كلب جيراني دائما بعوي بصوت عالي",
+        type: "vocabs",
+        level:"a2",
+        lan: "de",
+        options: [
+            {answer:"geht",isCorrect:false},
+            {answer:"bellt",isCorrect:true},
+            {answer:"kommt",isCorrect:false},
+            {answer:"raucht",isCorrect:false},
+        ]
+    },
+    {
+        question: "Ich habe keine __ auf diesen Film",
+        translate: "مالي مراء لهاد الفلم",
+        type: "vocabs",
+        level:"a2",
+        lan: "de",
+        options: [
+            {answer:"Lust",isCorrect:true},
+            {answer:"bock",isCorrect:false},
+            {answer:"jacke",isCorrect:false},
+            {answer:"schuhe",isCorrect:false},
+        ]
+    },
+    {
+        question: "Maria arbeitet als Lehrerin an einer __",
+        translate: "ماريا بتشتغل بمدرسة ابتدائي",
+        type: "vocabs",
+        level:"a2",
+        lan: "de",
+        options: [
+            {answer:"universität",isCorrect:false},
+            {answer:"hause",isCorrect:false},
+            {answer:"Grundschule",isCorrect:true},
+            {answer:"markt",isCorrect:false},
+        ]
+    },
+    {
+        question: "Mein Handy ist leider __ gegangen",
+        translate: "موبايلي انتزع",
+        type: "vocabs",
+        level:"a2",
+        lan: "de",
+        options: [
+            {answer:"kaputt",isCorrect:true},
+            {answer:"geht",isCorrect:false},
+            {answer:"sicher",isCorrect:false},
+            {answer:"gut",isCorrect:false},
+        ]
+    },
+    {
+        question: "Ich habe Hunger. Können wir bald etwas essen __?",
+        translate: "انا جوعان, فينا بعد شوي نروح ناكل شي ؟",
+        type: "vocabs",
+        level:"a2",
+        lan: "de",
+        options: [
+            {answer:"kommen",isCorrect:false},
+            {answer:"zu gehen",isCorrect:false},
+            {answer:"gehen",isCorrect:true},
+            {answer:"habe",isCorrect:false},
+        ]
+    },
+    {
+        question: "Können wir bitte eine Pizza __?",
+        translate: "فينا نطلب بيتزا؟",
+        type: "vocabs",
+        level:"a2",
+        lan: "de",
+        options: [
+            {answer:"singen",isCorrect:false},
+            {answer:"bestellen",isCorrect:true},
+            {answer:"gehen",isCorrect:false},
+            {answer:"kommen",isCorrect:false},
+        ]
+    },
+    {
+        question: "Die Schüler, die in der letzten __ sitzen, __ sich während des Unterrichts",
+        translate: "الطلاب, يلي قاعدين باخر صف, عم يحكو اثناء الدرس",
+        type: "vocabs",
+        level:"a2",
+        lan: "de",
+        options: [
+            {answer:"reihe,unterhalten",isCorrect:false},
+            {answer:"raum,sagen",isCorrect:false},
+            {answer:"sitz,sprechen",isCorrect:false},
+            {answer:"Reihe,unterhalten",isCorrect:true}
+        ]
+    },
+    {
+        question: "Ich habe gestern Abend ein __ Buch gelesen",
+        translate: "مبارح قرات كتاب مشوق",
+        type: "vocabs",
+        level:"a2",
+        lan: "de",
+        options: [
+            {answer:"spanendes",isCorrect:false},
+            {answer:"spannendes",isCorrect:true},
+            {answer:"schöne",isCorrect:false},
+            {answer:"schlechte",isCorrect:false},
+        ]
+    },
+    {
+        question: "Die Frau, arbeitet in der __",
+        translate: "المرأة تشتغل بالمكتبة",
+        type: "vocabs",
+        level:"a2",
+        lan: "de",
+        options: [
+            {answer:"Bibliothek",isCorrect:true},
+            {answer:"bibliothek",isCorrect:false},
+            {answer:"Bibliotek",isCorrect:false},
+            {answer:"bibliotek",isCorrect:false},
+        ]
+    },
+    {
+        question: "Meine Nachbarin hat gerade ihr Studium __",
+        translate: "جارتي خلصت هلا دراستها",
+        type: "vocabs",
+        level:"a2",
+        lan: "de",
+        options: [
+            {answer:"Beendet",isCorrect:false},
+            {answer:"beenden",isCorrect:false},
+            {answer:"beendet",isCorrect:true},
+            {answer:"bendet",isCorrect:false},
+        ]
+    },
+    {
+        question: "Ich habe keine __, wo meine Brille __. Ich habe __ gesucht",
+        translate: "ماعندي علم, وين نضارتي, دورت بكل مكان",
+        type: "vocabs",
+        level:"a2",
+        lan: "de",
+        options: [
+            {answer:"Ahnung,ist,überall",isCorrect:true},
+            {answer:"bock,sind,kein",isCorrect:false},
+            {answer:"Ahnung,sind,überall",isCorrect:false},
+            {answer:"ahnung,ist,überall",isCorrect:false},
+        ]
+    },
+    // ================================================= A2 Writting ======================================
+    {
+        question: "kocht/Meine/immer/besuche./Großmutter/Gerichte, wenn/ich/leckere/sie",
+        translate: "No Translate, Dont forget to put after the coma a space",
+        type: "writting",
+        level:"a2",
+        lan: "de",
+       answer:"Meine Großmutter kocht immer leckere Gerichte, wenn ich sie besuche."
+    },
+    {
+        question: "habe/Paar/waren./mir/Ich/ein/neues/heute/gekauft, weil/meine/Schuhe/kaputt/alten",
+        translate: "No Translate, Dont forget to put after the coma a space",
+        type: "writting",
+        level:"a2",
+        lan: "de",
+       answer:"Ich habe mir heute ein neues Paar Schuhe gekauft, weil meine alten kaputt waren."
+    },
+    {
+        question: "eine/Die/bauen/Höhle/aus/spielen/im/Garten/und/Kinder/Sand.",
+        translate: "No Translate",
+        type: "writting",
+        level:"a2",
+        lan: "de",
+       answer:"Die Kinder spielen im Garten und bauen eine Höhle aus Sand."
+    },
+    {
+        question: "Fenster/warm./du/bitte/ist/hier/drinnen/sehr/das/Kannst/öffnen? /Es",
+        translate: "No Translate, Dont forget to put a space after the question mark",
+        type: "writting",
+        level:"a2",
+        lan: "de",
+       answer:"Kannst du bitte das Fenster öffnen? Es ist hier drinnen sehr warm."
+    },
+    {
+        question: "gerade/Tafel./Lehrer/an/Mathematikaufgabe/erklärt/Der/die/der",
+        translate: "No Translate",
+        type: "writting",
+        level:"a2",
+        lan: "de",
+       answer:"Der Lehrer erklärt gerade die Mathematikaufgabe an der Tafel."
+    },
+    {
+        question: "6/werde/morgen/habe./früh/Ich/Termin/wichtigen/um/Uhr/aufstehen, weil/ich/einen",
+        translate: "No Translate, Dont forget to put after the coma a space",
+        type: "writting",
+        level:"a2",
+        lan: "de",
+       answer:"Ich werde morgen früh um 6 Uhr aufstehen, weil ich einen wichtigen Termin habe."
+    },
+    {
+        question: "oft/Wochenende/Freund/und/Kino./Mein/am/ich/gehen/ins",
+        translate: "No Translate",
+        type: "writting",
+        level:"a2",
+        lan: "de",
+       answer:"Mein Freund und ich gehen am Wochenende oft ins Kino."
+    },
+    {
+        question: "gebrochen/habe/es/das/Schwester/Ich/reparieren./Fahrad/meiner/und/muss/jetzt",
+        translate: "No Translate",
+        type: "writting",
+        level:"a2",
+        lan: "de",
+       answer:"Ich habe das Fahrad meiner Schwester gebrochen und muss es jetzt reparieren."
+    },
+    {
+        question: "Wetter/Sonne/heute/ist/zu/und/sehr/nicht/Das/kalt./schön./Die/scheint/es/ist",
+        translate: "No Translate",
+        type: "writting",
+        level:"a2",
+        lan: "de",
+       answer:"Das Wetter heute ist sehr schön. Die Sonne scheint und es ist nicht zu kalt."
+    },
+    {
+        question: "Schüler, die/Reihe/in/unterhalten/während/Unterrichts./der/Die/letzten/sitzen, /sich/des",
+        translate: "No Translate, Dont forget to put after the coma a space",
+        type: "writting",
+        level:"a2",
+        lan: "de",
+       answer:"Die Schüler, die in der letzten Reihe sitzen, unterhalten sich während des Unterrichts."
+    },
+  
 ]
 console.log(data.length);
