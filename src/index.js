f5a62aaeabb87e33c3be7405388f43c05914297c
atakriti <anwart256@gmail.com>
2023-01-18 15:41:54 +0100

    update

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import {BrowserRouter} from "react-router-dom"
 import Context from './Context';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Context>
@@ -12,3 +14,5 @@ root.render(
     </Context>
 );
 
+reportWebVitals();
+serviceWorkerRegistration.register();
